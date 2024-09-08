c3b90d46e30dea966ee06def9cde0570ecea6846
Navid-ab1 <navidab3113@gmail.com>
2024-09-08 11:40:12 +0330

    Add required field to the register forum

--- backend/public/javascripts/login.js
+++ backend/public/javascripts/login.js
@@ -9,26 +9,26 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     // Basic validation
     if (username === '' || password === '') {
         document.getElementById('error-message').textContent = 'Both fields are required.';
-    } else {
-        fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({username: username, password: password}),
-        })
-            .then(response => {
-                if (response.ok) {
-                    return response.text();
-                } else {
-                    throw new Error('Login failed');
-                }
-            })
-            .then(data => {
-                document.body.innerHTML =  data; // This will replace the body with the response content
-            })
-            .catch(error => {
-                document.getElementById('error-message').textContent = 'Login failed with ' + error.message;
-            });
+    // } else {
+    //     fetch('/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({username: username, password: password}),
+    //     })
+    //         .then(response => {
+    //             if (response.ok) {
+    //                 return response.text();
+    //             } else {
+    //                 throw new Error('Login failed');
+    //             }
+    //         })
+    //         .then(data => {
+    //             document.body.innerHTML =  data; // This will replace the body with the response content
+    //         })
+    //         .catch(error => {
+    //             document.getElementById('error-message').textContent = 'Login failed with ' + error.message;
+    //         });
     }
 });
---
+++ backend/public/javascripts/register.js
@@ -0,0 +1,11 @@
+document.getElementById('registerForm').addEventListener('submit', function (event)  {
+    event.preventDefault();
+    const Name = document.getElementById('name').value.trim();
+    const familyName = document.getElementById('family-name').value.trim();
+    const address = document.getElementById('address').value.trim();
+    const phoneNumber = document.getElementById('phoneNumber').value.trim();
+    const password = document.getElementById('password').value.trim();
+    if (Name.value === '' || familyName.value === '' || address.value === '' || phoneNumber.value === '' || password.value === '') {
+        document.getElementById('error-message').textContent = 'fields are required.';
+    }
+});
